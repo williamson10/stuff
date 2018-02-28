a207015b55f33cff67363b4e714e4aa75a12d9d1
Justin Williamson <williamson10@gmail.com>
2018-02-28 11:38:41 -0500

    Update radarr_installer.sh

--- radarr_installer.sh
+++ radarr_installer.sh
@@ -1,3 +1,5 @@
+# NLOG bug - https://github.com/Radarr/Radarr/issues/2585
+
 yum install epel-release yum-utils -y
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
 yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
